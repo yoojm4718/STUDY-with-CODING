2973325ca8d06a3953c80a5180fcd811d46e13d3
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-07-30 22:05:31 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,25 +1,19 @@
-//1929
+//1712
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include<stdbool.h>
+
+long long BEP(long long A, long long B, long long C);
 
 int main(void)
 {
-	long long M, N;
-	bool num[1000001] = { false };//00
-	scanf("%llu %llu", &M, &N);
-	num[2] = true;
-	for (long long i = 3;i < 1000000;i += 2)
-		num[i] = true;
-	for (long long i = 3;i < 1000000;i += 2)
-	{
-		for (long long j = i;;j += 2)
-		{
-			if (i * j >= 1000000)break;
-			else num[i * j] = false;
-		}
-	}
-	for (long long i = M;i <= N;i++)
-		if (num[i] == true)printf("%llu\n", i);
+	long long A, B, C;
+	scanf("%llu %llu %llu", &A, &B, &C);
+	if (BEP(A, B, C) >= 0)
+		printf("%llu", BEP(A, B, C));
+	else printf("-1");
 	return 0;
+}
+long long BEP(long long A, long long B, long long C) {
+	if (C <= B)return -1;
+	else return A / (C - B) + 1;
 }
