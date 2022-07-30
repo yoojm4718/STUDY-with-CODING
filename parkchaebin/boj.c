ccc7dba9f6cf1bdd41d6f0c2cac04ddc7c12d710
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-07-30 12:27:28 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,36 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <ctype.h>
 
 int main(void)
 {
-    int num[26] = { 0 }, max = 0, a = 0;
-    char san[1000000];
-    scanf("%s", san);
-    for (int i = 0;san[i];i++)
-    {
-        if (islower(san[i])) san[i] = toupper(san[i]);
-        num[san[i] - 65]++;
-    }
-    for (int k = 0; k < 26; k++)
-    {
-        if (max < num[k])
-            max = num[k];
-    }
-    for (int k = 0; k < 26; k++)
-    {
-        if (max == num[k])
-            a++;
-    }
-    if (a == 1)
-    {
-        for (int k = 0; k < 26; k++)
-        {
-            if (max == num[k])
-                printf("%c", k + 65);
-        }
-    }
-    else printf("?");
-
-    return 0;
+	int num, n1, n2, res;
+	scanf("%d", &num);
+	for (int i = 0;i < num;i++)
+	{
+		scanf("%d %d", &n1, &n2);
+		res = pow(n1, n2);
+		res = res % 10;
+		printf("%d", res);
+	}
+	return 0;
 }
