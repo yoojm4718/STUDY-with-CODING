dbb8568f45754fde5951e8d2c5a0d1407c1eaec0
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-08-01 13:38:31 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,23 +1,35 @@
-//2292
+//1316
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
-int hy(int N, int num);
+#include <string.h>
 
 int main(void)
 {
-	int num = 0;
-	long long N;
-	scanf("%llu", &N);
-	if (N == 1)printf("1");
-	else printf("%d", hy(N, num));
-	return 0;
-}
-int hy(int N, int num) {
-	for (int i = 0;;i++)
-	{
-		num += 6 * i;
-		if (N <= 1 + num)return i + 1;
+	int num, count = 0, n, re = 0;
+	char san[100];
+	scanf("%d", &num);
+	for (int i = 0;i < num;i++) {
+		n = 0;
+		scanf("%s", san);
+		for (int p = 0;san[p];p++)
+			if (san[p + 1] == 0) re = p + 1;
+		if (re > 2) {
+			for (int j = 2;j < re;j++)
+			{
+				for (int k = 0;k < j;k++)
+				{
+					if (san[j] == san[k] && san[j - 1] != san[j]) {
+						n--;
+						break;
+					}
+					if (k == j - 1)n++;
+				}
+			}
+			if (n == re - 2)count++;
+		}
+		else count++;
 	}
+	printf("%d", count);
 
+	return 0;
 }
