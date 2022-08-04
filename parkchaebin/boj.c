1dc650470c7628f841c21466bb12cff01050d1b2
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-08-04 20:25:08 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,24 +1,14 @@
-//2743
-
-#define _CRT_SECURE_NO_WARNINGS
+//10699
 
 #include <stdio.h>
+
+
 
 int main(void)
 
 {
 
-	char san[100];
-
-	int num = 0;
-
-	scanf("%s", san);
-
-	for (int i = 0; san[i]; i++)
-
-		num++;
-
-	printf("%d", num);
+	printf("2022-08-04");
 
 
 
