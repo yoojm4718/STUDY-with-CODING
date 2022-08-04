1a671744160c6c9f7afd62b4ca93a9586da599f2
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-08-04 20:24:34 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,14 +1,26 @@
-//7287
+//2743
+
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-
-
 
 int main(void)
 
 {
 
-	printf("77\nlooksambrook");
+	char san[100];
+
+	int num = 0;
+
+	scanf("%s", san);
+
+	for (int i = 0; san[i]; i++)
+
+		num++;
+
+	printf("%d", num);
+
+
 
 	return 0;
 
