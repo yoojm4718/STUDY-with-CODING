a20b87a133cec03409fc440a83be1c35ec2860b3
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-08-14 23:36:53 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,23 +1,11 @@
-//11942
-
+//3003
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-
-
 int main(void)
-
 {
-
-	printf("고려대학교");
-
-
-
-
-
-
-
-
-
+	int n1, n2, n3, n4, n5, n6;
+	scanf("%d %d %d %d %d %d", &n1, &n2, &n3, &n4, &n5, &n6);
+	printf("%d %d %d %d %d %d", 1 - n1, 1 - n2, 2 - n3, 2 - n4, 2 - n5, 8 - n6);
 	return 0;
-
 }
