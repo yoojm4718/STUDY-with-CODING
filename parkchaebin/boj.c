bbc6a6651ffe06110470a55f1128ec5a76f65198
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-08-22 09:28:13 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,11 +1,19 @@
-//3003
+//25304
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void)
 {
-	int n1, n2, n3, n4, n5, n6;
-	scanf("%d %d %d %d %d %d", &n1, &n2, &n3, &n4, &n5, &n6);
-	printf("%d %d %d %d %d %d", 1 - n1, 1 - n2, 2 - n3, 2 - n4, 2 - n5, 8 - n6);
+	long long x, a, sum = 0;
+	int n, b;
+	scanf("%llu", &x);
+	scanf("%d", &n);
+	for (int i = 0;i < n;i++)
+	{
+		scanf("%llu %d", &a, &b);
+		sum += (a * b);
+	}
+	if (x == sum)printf("Yes");
+	else printf("No");
 	return 0;
 }
