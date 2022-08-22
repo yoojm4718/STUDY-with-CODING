d7d853fc2943b2fa46c223632d7781b5496eb87b
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-08-22 10:08:23 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,19 +1,32 @@
-//25304
+//9020
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void)
 {
-	long long x, a, sum = 0;
-	int n, b;
-	scanf("%llu", &x);
-	scanf("%d", &n);
-	for (int i = 0;i < n;i++)
+	int T, num, n, p[10001] = { 0 }, a, b;
+	scanf("%d", &T);
+	p[2] = 1;
+	for (int i = 3;i < 10001;i += 2)
+		p[i] = 1;
+	for (int i = 3;i < 10001;i += 2)
 	{
-		scanf("%llu %d", &a, &b);
-		sum += (a * b);
+		for (int j = 3;j <= i;j += 2)
+			if (i * j <= 10000)
+				p[i * j] = 0;
 	}
-	if (x == sum)printf("Yes");
-	else printf("No");
+	for (int i = 0;i < T;i++)
+	{
+		num = 2;
+		scanf("%d", &n);
+		while (n >= 2 * num)
+		{
+			if (p[num] == 1 && p[n - num] == 1)
+				a = num, b = n - num;
+			num++;
+		}
+		printf("%d %d\n", a, b);
+	}
+
 	return 0;
 }
