5ed82d91065ef414116ebb9eb12ae92cf407aa87
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-08-26 11:14:50 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,29 +1,24 @@
-//2920
+//10807
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
-	int num[8], count = 0, sum = 0, se = 0;
-	for (int i = 0;i < 8;i++)
+	unsigned int N;
+	int num[100], v, count = 0, len = 0;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
 	{
 		scanf("%d", &num[i]);
 	}
-	for (int i = 0;i < 8;i++) {
-		se++;
-		if (num[i] == i + 1) {
-			count++;
-			if (count == 7)printf("ascending");
-			else if (se == 7)printf("mixed");
-		}
-		else if (num[i] == (8 - i)) {
-			sum++;
-			if (sum == 7)printf("descending");
-			else if (se == 7)printf("mixed");
-		}
-		else if (se == 7)printf("mixed");
+	scanf("%d", &v);
+	for (int i = 0; i < N; i++)
+	{
+		if (num[i] == v)count++;
 	}
+	printf("%d", count);
+
 
 	return 0;
 
