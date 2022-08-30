dad20188102b848b8bb1d976069644ad33769822
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-08-30 19:05:46 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,19 +1,12 @@
-//9086
+//15964
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
-	int T, len;
-	char san[100];
-	scanf("%d", &T);
-	for (int i = 0;i < T;i++)
-	{
-		scanf("%s", san);
-		len = strlen(san);
-		printf("%c%c\n", san[0], san[len - 1]);
-	}
+	long long A, B;
+	scanf("%lld %lld", &A, &B);
+	printf("%lld", (A + B) * (A - B));
 
 	return 0;
 }
