52555653edd23d034786a765bfc49093b3dcae61
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-08-30 18:49:52 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,18 +1,32 @@
-//2744
+//2754
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <ctype.h>
 
 int main(void)
 {
-	char san[100];
+	char san[3];
 	scanf("%s", san);
-	for (int i = 0;san[i];i++)
-	{
-		if (isupper(san[i]) != 0) san[i] = tolower(san[i]);
-		else if (islower(san[i]) != 0) san[i] = toupper(san[i]);
+	if (san[0] == 'A') {
+		if (san[1] == '+')printf("4.3");
+		else if (san[1] == '-')printf("3.7");
+		else printf("4.0");
 	}
-	printf("%s", san);
+	else if (san[0] == 'B') {
+		if (san[1] == '+')printf("3.3");
+		else if (san[1] == '-')printf("2.7");
+		else printf("3.0");
+	}
+	else if (san[0] == 'C') {
+		if (san[1] == '+')printf("2.3");
+		else if (san[1] == '-')printf("1.7");
+		else printf("2.0");
+	}
+	else if (san[0] == 'D') {
+		if (san[1] == '+')printf("1.3");
+		else if (san[1] == '-')printf("0.7");
+		else printf("1.0");
+	}
+	else printf("0.0");
 
 	return 0;
 }
