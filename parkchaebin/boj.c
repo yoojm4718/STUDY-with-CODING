99eca1a2c061242c0e05087242b1bb5bdc3bd653
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-08-30 18:20:10 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,14 +1,33 @@
-//11382
+//2738
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void)
 {
-	unsigned long long A[1], B[1], C[1], sum[1];
-	scanf("%llu %llu %llu", &A[0], &B[0], &C[0]);
-	sum[0] = A[0] + B[0] + C[0];
-	printf("%llu", sum[0]);
-
+	unsigned int N, M;
+	int A[100][100], B[100][100];
+	scanf("%u %u", &N, &M);
+	for (int i = 0;i < N;i++)
+	{
+		for (int j = 0;j < M;j++)
+		{
+			scanf("%d", &A[i][j]);
+		}
+	}
+	for (int i = 0;i < N;i++)
+	{
+		for (int j = 0;j < M;j++)
+		{
+			scanf("%d", &B[i][j]);
+		}
+	}
+	for (int i = 0;i < N;i++)
+	{
+		for (int j = 0;j < M;j++)
+		{
+			printf("%d ", A[i][j] + B[i][j]);
+		}
+		puts("");
+	}
 	return 0;
-
 }
