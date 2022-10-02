0300004af2f1ec0e74812ec625e2a40e0a3d8973
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-03 03:45:00 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,30 +1,15 @@
-//1924
+//11050
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void)
 {
-	int x, y, n = 0;
-	int A[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
-
-	scanf("%d %d", &x, &y);
-
-	for (int i = 1;i < x;i++)
-		n += A[i];
-	n += y;
-	n %= 7;
-	switch (n)
-	{
-	case 0: {printf("SUN");break;}
-	case 1: {printf("MON");break;}
-	case 2: {printf("TUE");break;}
-	case 3: {printf("WED");break;}
-	case 4: {printf("THU");break;}
-	case 5: {printf("FRI");break;}
-	case 6: {printf("SAT");break;}
-	default:
-		break;
-	}
-
+	int N, K, count = 1;
+	scanf("%d %d", &N, &K);
+	for (int i = K + 1;i <= N;i++)
+		count *= i;
+	for (int i = 2;i <= (N - K);i++)
+		count /= i;
+	printf("%d", count);
 	return 0;
 }
