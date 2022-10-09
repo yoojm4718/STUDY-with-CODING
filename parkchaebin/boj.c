b0127464ad025d4b9962d18c6bd455e9722570bd
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-10 02:42:07 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,21 +1,32 @@
-//11655
+//2750
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-	char san[100];
-	gets(san);
-	for (int i = 0;san[i];i++)
+	int N, i, j, A[1000], tmp;
+
+	scanf("%d", &N);
+
+	for (i = 0;i < N;i++)
 	{
-		if ((san[i] >= 'a' && san[i] <= 'z') || (san[i] >= 'A' && san[i] <= 'Z'))
-			if (((san[i] + 13) > 'Z' && san[i] <= 'Z') || ((san[i] + 13) > 'z' && san[i] <= 'z'))
-				printf("%c", san[i] - 13);
-			else
-				printf("%c", san[i] + 13);
-		else
-			printf("%c", san[i]);
+		scanf("%d", &A[i]);
 	}
 
+	for (i = 0;i < N;i++)
+	{
+		for (j = i;j < N;j++)
+		{
+			if (A[i] > A[j]) {
+				tmp = A[i];
+				A[i] = A[j];
+				A[j] = tmp;
+			}
+		}
+	}
+
+	for (i = 0;i < N;i++)
+		printf("%d\n", A[i]);
 	return 0;
 }
