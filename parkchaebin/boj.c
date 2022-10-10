6be3cdf57143e5f8e4063a351331450dc551d651
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-11 00:18:37 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,35 +1,13 @@
-//25501
+//2163
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 
-int c1 = 0, c2 = 0;
-int recursion(const char* s, int l, int r) {
-	c1++;
-	if (l >= r) return 1;
-	else if (s[l] != s[r]) return 0;
-	else return recursion(s, l + 1, r - 1);
-}
-
-int isPalindrome(const char* s) {
-	c2 = recursion(s, 0, strlen(s) - 1);
-	return recursion(s, 0, strlen(s) - 1);
-}
-
 int main(void)
 {
-	int N, i;
-	char sep[1000];
-
-	scanf("%d", &N);
-
-	for (i = 0;i < N;i++)
-	{
-		scanf("%s", sep);
-		isPalindrome(sep);
-		printf("%d %d\n", c2, c1 / 2);
-		c1 = 0, c2 = 0;
-	}
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d", A * B - 1);
 
 	return 0;
 }
