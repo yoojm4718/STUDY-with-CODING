1dcdf0dc161de0693d91c5bbf452cc2b2acc910f
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-17 03:47:03 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,13 +1,19 @@
-//2163
+//5543
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
+#include<stdlib.h>
+int main(void) {
 
-int main(void)
-{
-	int A, B;
-	scanf("%d %d", &A, &B);
-	printf("%d", A * B - 1);
+	int N[5], sum = 0;
+	for (int i = 0;i < 5;i++)
+		scanf("%d", &N[i]);
+	if (N[0] <= N[1] && N[0] <= N[2])sum += N[0];
+	else if (N[1] <= N[0] && N[1] <= N[2])sum += N[1];
+	else if (N[2] <= N[1] && N[2] <= N[0])sum += N[2];
 
+	if (N[3] <= N[4])sum += N[3];
+	else sum += N[4];
+
+	printf("%d", sum - 50);
 	return 0;
 }
