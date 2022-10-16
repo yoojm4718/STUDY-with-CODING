f7ea6a75fbb1882ffe556788fd399d15ba741da9
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-17 04:14:19 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,16 +1,12 @@
-//17388
+//15963
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include<stdlib.h>
 int main(void) {
 
-	int a[3];
-	scanf("%d %d %d", &a[0], &a[1], &a[2]);
-	if (a[0] + a[1] + a[2] >= 100)printf("OK");
-	else {
-		if (a[0] <= a[1] && a[0] <= a[2])printf("Soongsil");
-		if (a[1] <= a[0] && a[1] <= a[2])printf("Korea");
-		if (a[2] <= a[1] && a[2] <= a[0])printf("Hanyang");
-	}
+	int a, b;
+	scanf("%d %d", &a, &b);
+	if (a == b)printf("1");
+	else printf("0");
 	return 0;
 }
