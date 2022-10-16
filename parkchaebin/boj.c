7926215c6e0afba3ff05600aceef82c932b524a3
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-17 04:12:11 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,12 +1,16 @@
-//24883
+//17388
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include<stdlib.h>
 int main(void) {
 
-	char a;
-	scanf("%c", &a);
-	if (a == 'N' || a == 'n')printf("Naver D2");
-	else printf("Naver Whale");
+	int a[3];
+	scanf("%d %d %d", &a[0], &a[1], &a[2]);
+	if (a[0] + a[1] + a[2] >= 100)printf("OK");
+	else {
+		if (a[0] <= a[1] && a[0] <= a[2])printf("Soongsil");
+		if (a[1] <= a[0] && a[1] <= a[2])printf("Korea");
+		if (a[2] <= a[1] && a[2] <= a[0])printf("Hanyang");
+	}
 	return 0;
 }
