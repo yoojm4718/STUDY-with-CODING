2849b4b36390c28c1c47399c18fd447b8dbe3269
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-17 04:04:16 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,11 +1,15 @@
-//3046
+//10808
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include<stdlib.h>
 int main(void) {
 
-	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d", 2 * b - a);
+	int c[26] = { 0 };
+	char san[100];
+	scanf("%s", san);
+	for (int i = 0;san[i];i++)
+		c[san[i] - 'a']++;
+	for (int i = 0;i < 26;i++)
+		printf("%d ", c[i]);
 	return 0;
 }
