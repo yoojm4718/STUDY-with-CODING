f833f7821f8efa87161dcda315cb014698f723c2
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-29 11:41:53 +0900

    Update boj.c

--- parkchaebin/boj.c
+++ parkchaebin/boj.c
@@ -1,12 +1,30 @@
-//15963
+//9012
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include<stdlib.h>
+
 int main(void) {
 
-	int a, b;
-	scanf("%d %d", &a, &b);
-	if (a == b)printf("1");
-	else printf("0");
+	int count = 0, sum = 0, T;
+	char san[51];
+
+	scanf("%d", &T);
+	for (int i = 0;i < T;i++)
+	{
+		count = 0;
+		sum = 0;
+
+		scanf("%s", san);
+		for (int j = 0;san[j];j++)
+		{
+			if (san[j] == '(')count++;
+			else count--;
+			if (count < 0) sum = -100;
+		}
+		if (sum != -100)sum = count;
+		if (sum == 0)printf("YES\n");
+		else printf("NO\n");
+	}
+
 	return 0;
 }
